14dadb48f213576788d9efabe61e7c9523b3050a
hexcrasher <serialstorm@yahoo.com>
2013-02-17 18:06:05 -0500

    why does git hate me

--- signal33_site/template_ext/form.py
+++ signal33_site/template_ext/form.py
@@ -1,22 +1,27 @@
 from cStringIO import StringIO
+from signal33_site import app
 
 FORM_URL_ENCODED = "application/x-www-form-urlencoded"
 FORM_FILE_UPLOAD = "multipart/form-data"
 
+@app.context_processor
 def begin_form(id, action, method="post", encoding_type=None):
     enctype = ""
     if encoding_type is not None:
         enctype = ' enctype="{0}"'.format(encoding_type)
-    return '<form id="{0}" name="{0} action="{1}" method="{2}"{3}>'.format(id, action, method, enctype)
-
+    return '<form id="{0}" name="{0}" action="{1}" method="{2}"{3}>'.format(id, action, method, enctype)
+@app.context_processor
 def end_form():
     return "</form>"
 
+@app.context_processor
 def form_input(id, type="text", **attributes):
     attrs = StringIO()
+
+    attrs.write(' ')
 
     for key in attributes.keys():
         attrs.write('{0}="{1} " '.format(key, attributes[key]))
 
-    return '<input type="{0}" id="{1} name="{1}"{2}/>'.format(id, type, attrs.getvalue())
+    return '<input type="{0}" id="{1}" name="{1}"{2}/>'.format(type, id, attrs.getvalue())
 
