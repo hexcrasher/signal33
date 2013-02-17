9197140bfaae76dae8683ec3ce746613c06cc2f2
hexcrasher <serialstorm@yahoo.com>
2013-02-17 18:01:31 -0500

    unit tests for form helper

---
+++ signal33_tests/signal33_site_tests/template_ext_tests/formtests.py
@@ -0,0 +1,17 @@
+from signal33_site.template_ext import form
+import unittest
+
+class FormTest(unittest.TestCase):
+
+    def test_begin_form_valid(self):
+        ret = form.begin_form('test', "test")
+        self.assertEqual(ret, '<form id="test" name="test" action="test" method="post">')
+
+    def test_end_form(self):
+        self.assertEqual(form.end_form(), "</form>")
+
+    def test_input(self):
+        self.assertEqual(form.form_input('test'), '<input type="text" id="test" name="test" />')
+
+
+unittest.main()
